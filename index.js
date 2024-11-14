e276306cfc4fe1fa7a469c33d422a752d0f77d10
Daysia Lagoa <daysia.lagoa@gmail.com>
2024-11-13 20:33:33 -0500

    project changes

---
+++ index.js
@@ -0,0 +1,8 @@
+import prompt from "picoprompt";
+import wordBank from "./word-bank.js";
+
+//console.log("Hello!");
+// const answer = prompt("How is your day?");
+// console.log(answer);
+
+console.log(wordBank);
