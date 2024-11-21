a461e57dfa19c26350572a48a63617ee32d9c11e
Daysia Lagoa <daysia.lagoa@gmail.com>
2024-11-20 21:05:42 -0500

    hangman project first draft

--- README.md
+++ README.md
@@ -7,3 +7,31 @@ Update me with a description of:
 - How do I play the game?
 
 You can see the [Markdown cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet) on how to format your README.md file.
+
+
+# Hangman Game
+
+This is a command-line Hangman game built with Node.js.
+
+## How to Play
+
+- Start the game with `node .`
+- Guess the word by entering one letter at a time.
+- The game will show you the current state of the word, which letters you've guessed, and how many guesses you have left.
+- You can stop the game at any time by pressing `ctrl + c`.
+
+## Requirements
+
+- Node.js
+- picoprompt library (`npm install picoprompt`)
+
+## Instructions to Start
+
+1. Clone the repository.
+2. Navigate to the project directory.
+3. Install the dependencies: `npm install`
+4. Start the game: `node .`
+
+## Author
+
+Daysia Lagoa - (https://github.com/DaysiaLagoa/HangmanProject)
--- index.js
+++ index.js
@@ -1,8 +1,87 @@
 import prompt from "picoprompt";
 import wordBank from "./word-bank.js";
+import { hangmanStages, welcomeMessage } from "./assets/ascii-art.js";
 
-//console.log("Hello!");
-// const answer = prompt("How is your day?");
-// console.log(answer);
+const getRandomWord = () => {
+    const randomIndex = Math.floor(Math.random() * wordBank.length);
+    return wordBank[randomIndex];
+};
 
-console.log(wordBank);
+const initializeDisplay = (word) => {
+    return "_".repeat(word.length).split("");
+};
+
+const updateDisplay = (display, word, letter) => {
+    word.split("").forEach((char, index) => {
+        if (char.toLowerCase() === letter.toLowerCase()) {
+            display[index] = char;
+        }
+    });
+};
+
+const checkGuess = (word, letter) => {
+    return word.toLowerCase().includes(letter.toLowerCase());
+};
+
+const isGameOver = (display, guesses) => {
+    return !display.includes("_") || guesses <= 0;
+};
+
+const playHangman = () => {
+    const word = getRandomWord();
+    let display = initializeDisplay(word);
+    let guessesLeft = 6;
+    let guessedLetters = [];
+    let wins = 0;
+    let losses = 0;
+
+    console.log(welcomeMessage);
+
+    const playRound = () => {
+        console.log(hangmanStages[6 - guessesLeft]);
+        console.log(`\nCurrent word: ${display.join(" ")}\n`);
+        console.log(`Guessed letters: ${guessedLetters.join(", ")}`);
+        console.log(`Guesses left: ${guessesLeft}\n`);
+
+        const guess = prompt("Please guess a letter: ");
+
+        if (guessedLetters.includes(guess.toLowerCase())) {
+            console.log("You already guessed that letter. Try again.");
+            return playRound();
+        }
+
+        guessedLetters.push(guess.toLowerCase());
+
+        if (checkGuess(word, guess)) {
+            updateDisplay(display, word, guess);
+        } else {
+            guessesLeft--;
+            console.log("Incorrect guess. Try again.");
+        }
+
+        if (!isGameOver(display, guessesLeft)) {
+            playRound();
+        } else {
+            if (guessesLeft > 0) {
+                console.log(`\nCongratulations! You guessed the word: ${word}`);
+                wins++;
+            } else {
+                console.log(hangmanStages[6]);
+                console.log(`\nGame over. The word was: ${word}`);
+                losses++;
+            }
+
+            console.log(`\nWins: ${wins} | Losses: ${losses}`);
+            const playAgain = prompt("Would you like to play again? (yes/no): ").toLowerCase();
+            if (playAgain === 'yes') {
+                playHangman();
+            } else {
+                console.log("Thanks for playing!");
+            }
+        }
+    };
+
+    playRound();
+};
+
+playHangman();
